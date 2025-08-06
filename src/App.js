d0e0e12339154680a0894355c8d28f4c0cb09929
jfgoyes <jfgoyes@espe.edu.ec>
2025-08-06 04:17:43 -0500

    Preparación archivos del proyecto

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import { useState, useEffect, useMemo } from "react";
+import ContactForm from "./components/FormularioContacto";
+import ContactCard from "./components/VisualizacionContacto";
+import ConfirmModal from "./components/ModalConfirmacion";
+
+function App() {
+
+}
+
+export default App;
