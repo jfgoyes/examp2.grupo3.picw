f3abb9754050b7a9f862ebf565f47b1947d70e83
tufita2003 <tufita2003@gmail.com>
2025-08-06 18:17:35 -0500

    Integracion del App.js iniciales

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// Parte 5: Torres Diaz Tiffani Nathalia
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
@@ -7,7 +9,38 @@ import ContactCard from "./components/VisualizacionContacto";
 import ConfirmModal from "./components/ModalConfirmacion";
 
 function App() {
+  const [contactos, setContactos] = useState(() => {
+    const guardados = localStorage.getItem("contactos");
+    return guardados ? JSON.parse(guardados) : [];
+  });
 
-}
+  const [contactoEditar, setContactoEditar] = useState(null);
+  const [contactoEliminar, setContactoEliminar] = useState(null);
+  const [modoOscuro, setModoOscuro] = useState(() => {
+    const modoGuardado = localStorage.getItem('modoOscuro');
+    return modoGuardado === 'true' ? true : false;
+  });
 
-export default App;
+  const [busqueda, setBusqueda] = useState("");
+  const [orden, setOrden] = useState("az"); // "az" o "za"
+  const [filtroLetra, setFiltroLetra] = useState(null); // null o "A", "B", ...
+
+  useEffect(() => {
+    localStorage.setItem('modoOscuro', modoOscuro);
+  }, [modoOscuro]);
+
+  useEffect(() => {
+    localStorage.setItem("contactos", JSON.stringify(contactos));
+  }, [contactos]);
+
+  const agregarContacto = (nuevo) => {
+    if (contactoEditar) {
+      const actualizados = contactos.map((c) =>
+        c.id === contactoEditar.id ? { ...nuevo, id: contactoEditar.id } : c
+      );
+      setContactos(actualizados);
+      setContactoEditar(null);
+    } else {
+      setContactos([...contactos, { ...nuevo, id: Date.now() }]);
+    }
+  };
